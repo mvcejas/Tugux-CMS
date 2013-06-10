0e6c665a6db3c29b1b2d96b130a11c7c2975fce9
Yousaf <mmesunny@gmail.com>
2013-06-10 19:57:53 +0200

    Remove duplicated directory

--- cms/administrator/menu.php
+++
@@ -1,8 +0,0 @@
-<!-- content menu starts-->
-<div class=" shortcuts"><a href="http://www.templates.tugux.com/" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image20','','images/shortcut_website_hover.jpg',1)"><img src="images/shortcut_website.jpg" alt="webdesign" name="Image20" width="320" height="270" border="0" id="Image20" /></a>
-
-  <a href="page.php?pid=29" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image21','','images/shortcut_graphics_hover.jpg',1)"><img src="images/shortcut_graphics.jpg" alt="graphic design" name="Image21" width="320" height="270" border="0" id="Image21" /></a>
-  
-  <a href="contact.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image12','','images/shortcut_contact_hover.jpg',1)"><img src="images/shortcut_contact.jpg" alt="Contact" name="Image12" width="320" height="270" border="0" id="Image12" /></a>
-  </div>
-<!-- content menu ends-->
